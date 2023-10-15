cec347c939b78e270c708e3138e7f7f4d9b7267a
Gian <giansantos.dev@gmail.com>
2023-10-15 01:29:08 -0300

    initial and last commit?

--- app/src/main/java/br/com/igorbag/githubsearch/data/GitHubService.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/data/GitHubService.kt
@@ -6,8 +6,6 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface GitHubService {
-
     @GET("users/{user}/repos")
     fun getAllRepositoriesByUser(@Path("user") user: String): Call<List<Repository>>
-
 }
